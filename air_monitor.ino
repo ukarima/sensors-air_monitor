d8cac7f4785ead3b47a40bba5ef5de96bc3f17b4
ukarima <ukarimaa@gmail.com>
2022-07-06 23:47:29 +0700

    bmp280 default output

--- air_monitor.ino
+++ air_monitor.ino
@@ -1,7 +1,13 @@
 //bmp280 I2C
 //esp32 SCL 22, SDA 21
 #include <Adafruit_BMP280.h>
+
 Adafruit_BMP280 bmp;
+unsigned bmpStatus;
+
+float temperature = 0;
+float pressure = 0;
+float approxAltitude = 0;
 
 //mq135
 #include <MQUnifiedsensor.h>
@@ -31,28 +37,15 @@ void setup() {
   Serial.begin(9600);
 
   //bmp280
-  pinMode(ledPower,OUTPUT);
-  while ( !Serial ) delay(100);   // wait for native usb
-  Serial.println(F("BMP280 test"));
-  unsigned status;
-  status = bmp.begin(0x76);
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
-    while (1) delay(10);
-  }
-
+  pinMode(ledPower,OUTPUT);  
+  bmpStatus = bmp.begin(0x76);
   /* Default settings from datasheet. */
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                  
                   
   //mq135 setting math model to calculate the PPM concentration and the value of constants & calibration
   MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
@@ -92,13 +85,6 @@ void loop() {
   // Chris Nafis (c) 2012
   dustDensity = 0.17 * calcVoltage - 0.1;
   
-  
-//  Serial.print("Raw Signal Value (0-1023) ");
-//  Serial.print(voMeasured);
-//  
-//  Serial.print(" - Voltage ");
-//  Serial.println(calcVoltage);
-//  
 //  Serial.print("Dust Density = ");
 //  Serial.print(dustDensity); // unit: mg/m3
 //  Serial.println(" mg/m3");
@@ -108,18 +94,23 @@ void loop() {
   Serial.print(dustDensity * 1000); // unit: mg/m3
   Serial.println(" ug/m3");
   
-
   //bmp280
+  if (bmpStatus){
+    temperature = bmp.readTemperature();
+    pressure = bmp.readPressure();
+    approxAltitude = bmp.readAltitude(1013.25); /* Adjusted to local forecast! */
+  }
+  
   Serial.print(F("Temperature = "));
-  Serial.print(bmp.readTemperature());
+  Serial.print(temperature);
   Serial.println(" *C");
 
   Serial.print(F("Pressure = "));
-  Serial.print(bmp.readPressure());
+  Serial.print(pressure);
   Serial.println(" Pa");
 
   Serial.print(F("Approx altitude = "));
-  Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+  Serial.print(approxAltitude); 
   Serial.println(" m");
 
   /*
